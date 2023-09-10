3605dbd5ab56d82bc1309a1d239cf76315838827
moesnow <11678347+moesnow@users.noreply.github.com>
2023-09-10 16:38:07 +0800

    feat: use QuoteReply

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
-    val kotlinVersion = "1.5.30"
+    val kotlinVersion = "1.8.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.9.2"
+    id("net.mamoe.mirai-console") version "2.15.0"
 }
 
-group = "top.imaikuai"
-version = "1.0-SNAPSHOT"
+group = "top.kotori"
+version = "1.0.0"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
     mavenCentral()
 }
 dependencies {
-    implementation("org.jsoup:jsoup:1.14.3")
-    implementation("com.alibaba:fastjson:1.2.80")
+    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("com.alibaba:fastjson:1.2.83")
 }
--- src/main/kotlin/KeywordReply.kt
+++ src/main/kotlin/KeywordReply.kt
@@ -5,21 +5,23 @@ import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.value
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.contact.Group
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.events.GroupMessageEvent
-import net.mamoe.mirai.message.data.At
+import net.mamoe.mirai.message.data.MessageChain
+import net.mamoe.mirai.message.data.MessageSource.Key.quote
 import net.mamoe.mirai.utils.info
 import org.jsoup.Jsoup
 
 object KeywordReply : KotlinPlugin(
     JvmPluginDescription(
-        id = "top.imaikuai.KeywordReply",
+        id = "top.kotori.KeywordReply",
         name = "Keyword Reply",
-        version = "1.0-SNAPSHOT",
+        version = "1.0.0",
     )
 ) {
     object KeywordReply : AutoSavePluginConfig("KeywordReply") {
-        val groupid by value("123456789")
+        val groupId by value("123456789")
         val Keyword: Map<String, String> by value(
             mapOf(
                 "hello" to "hi",
@@ -30,24 +32,45 @@ object KeywordReply : KotlinPlugin(
     }
 
     override fun onEnable() {
+        // 重新加载关键词配置
         KeywordReply.reload()
         logger.info { "Keyword Reply loaded" }
+
+        // 订阅群组消息事件
         GlobalEventChannel.subscribeAlways<GroupMessageEvent> { event ->
-            if (sender.group.id.toString() == KeywordReply.groupid) {
-                if (KeywordReply.hitokoto && message.contentToString() == "一言") {
-                    val html = Jsoup.connect("https://v1.hitokoto.cn/?encode=json").ignoreContentType(true).get()
-                    val hitokoto = JSON.parseObject(html.text())
-                    val at = At(event.sender.id)
-                    subject.sendMessage(at + (hitokoto.getString("hitokoto") + "\n来源：" + hitokoto.getString("from")))
+            val groupId = event.sender.group.id.toString()
+            val messageContent = event.message.contentToString()
+
+            if (groupId == KeywordReply.groupId) {
+                if (KeywordReply.hitokoto && messageContent == "一言") {
+                    // 处理一言请求
+                    handleHitokoto(event.subject, event.message)
                 } else {
-                    for ((key, value) in KeywordReply.Keyword) {
-                        if (message.contentToString().contains(key)) {
-                            subject.sendMessage(value)
-                            break
-                        }
-                    }
+                    // 处理关键词回复
+                    handleKeywordReply(event.subject, event.message)
                 }
             }
         }
     }
+
+    private suspend fun handleHitokoto(subject: Group, message: MessageChain) {
+        val html = Jsoup.connect("https://v1.hitokoto.cn/?encode=json")
+            .ignoreContentType(true)
+            .get()
+        val hitokoto = JSON.parseObject(html.text())
+        val replyMessage = message.quote() +
+                "${hitokoto.getString("hitokoto")}\n来源：${hitokoto.getString("from")}"
+        subject.sendMessage(replyMessage)
+    }
+
+    private suspend fun handleKeywordReply(subject: Group, message: MessageChain) {
+        val messageContent = message.contentToString()
+        for ((key, value) in KeywordReply.Keyword) {
+            if (messageContent.contains(key)) {
+                subject.sendMessage(message.quote() + value)
+                break
+            }
+        }
+    }
+
 }
